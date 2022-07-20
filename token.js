3c5f5a7c8e55c4b56eae898b1e98b463a2b4bf98
Cheikh Ahmed <chaloueimin@gmail.com>
2022-07-20 09:47:45 +0000

    message

---
+++ token.js
@@ -0,0 +1,7 @@
+{
+    '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'
+}
+
+// registry=https://registry.npmjs.org/
+// @cheikhrim8:registry=https://npm.pkg.github.com/
+// npm.pkg.github.com/:_authToken=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
